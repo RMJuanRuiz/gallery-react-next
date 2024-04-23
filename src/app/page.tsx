1ad4698d24b6131a814227fa1f76141c2adb9813
Juan Ruiz <rmjuanruiz@gmail.com>
2024-04-23 12:14:28 -0500

    Setup database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/ab12ad88-5a47-4f59-8d96-cf3cdc8ef63b-6tyo61.jpeg",
   "https://utfs.io/f/2b1fd047-2185-4e04-97e6-161f5e29d6f4-1ahcsj.jpeg",
@@ -10,10 +12,17 @@ const mockImages = mockUrls.map((url, idx) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
