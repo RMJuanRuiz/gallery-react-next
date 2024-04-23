817923f72e48ea64f86b6bcd23fe7441dc02b4ea
Juan Ruiz <rmjuanruiz@gmail.com>
2024-04-23 16:17:23 -0500

    Break queries into a separate file

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,19 +6,20 @@ import { images } from "~/server/db/schema";
 
 const f = createUploadthing();
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 40 } })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
       const user = auth();
-
-      // If you throw, the user will not be able to upload
       if (!user.userId) throw new UploadThingError("Unauthorized");
+      /* 
+      const fullUserData = await clerkClient.users.getUser(user.userId);
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      if (fullUserData?.privateMetadata?.["can-upload"] !== true)
+        throw new UploadThingError("User Does Not Have Upload Permissions");
+
+      const { success } = await ratelimit.limit(user.userId);
+      if (!success) throw new UploadThingError("Ratelimited");
+ */
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
