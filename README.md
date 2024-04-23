4e2a7d959f5203efa957d7c172e29184115d973f
Juan Ruiz <rmjuanruiz@gmail.com>
2024-04-23 11:49:52 -0500

    Add initial scheleton

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## TO DO
 
-- [] Make it deploy (vercel)
+- [x] Make it deploy (vercel)
 - [] Scaffold basic ui with mock data
 - [] Tidy up build process
 - [] Actually set up a database (vercel postgres)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/ab12ad88-5a47-4f59-8d96-cf3cdc8ef63b-6tyo61.jpeg",
+  "https://utfs.io/f/2b1fd047-2185-4e04-97e6-161f5e29d6f4-1ahcsj.jpeg",
+  "https://utfs.io/f/21b04ce6-e20d-4ee8-b1c5-3f399a97d72b-1ahcsh.jpeg",
+  "https://utfs.io/f/3cfd978c-66f2-494c-afa7-58b70e5c6c50-1ahcsi.jpeg",
+];
+
+const mockImages = mockUrls.map((url, idx) => ({
+  id: idx + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Its working
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
